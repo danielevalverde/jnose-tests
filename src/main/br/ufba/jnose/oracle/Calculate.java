43b86dcdd053668afe320014ab9ccbbb5ddda027
danielevalverde <danierzevalverde@gmail.com>
2020-10-07 20:09:59 -0300

    add new tests and improve test names

--- src/main/br/ufba/jnose/oracle/Calculate.java
+++ src/main/br/ufba/jnose/oracle/Calculate.java
@@ -2,11 +2,21 @@ package br.ufba.jnose.oracle;
 
 public class Calculate {
 	
+	public int initial_value;
+	public String state;
+	
+	public Calculate(String state) {
+		initial_value = 0; 
+		this.state = state;
+	}
+	
 	public String turnOn() {
+		this.state = "on";
 		return "Turn On";
 	}
 	
 	public String turnOff() {
+		this.state = "off";
 		return "Turn Off";
 	}
 	
--- src/test/br/ufba/jnose/oracle/CalculateTest.java
+++ src/test/br/ufba/jnose/oracle/CalculateTest.java
@@ -1,6 +1,12 @@
 package br.ufba.jnose.oracle;
 
+import br.ufba.jnose.oracle.Calculate;  
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
 
 import org.hamcrest.Matcher;
 import org.junit.Before;
@@ -11,96 +17,98 @@ public class CalculateTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		
+		String state = "on";
 	}
-	private final Calculate calc;
+	private Calculate calc;
 	
 	public CalculateTest() {
-		calc = new Calculate();
+		calc = new Calculate("off");
 	}
 		
 	@Test
-	public void default_Test() {
+	public void should_be_default_Test() {
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void default_test_2() throws InterruptedException {
+	public void should_be_default_test_2() throws InterruptedException {
 		/* following the example on https://testsmells.github.io/pages/testsmellexamples.html#DefaultTest*/
-		Calculate calcule = new Calculate();
+		Calculate calcule = new Calculate("on");
 		int actual = calcule.add(2, 3);
 		int expected = 2;
 		assertEquals(expected,actual);
 	}
 	
 	@Test
-	public void default_test_3() throws Exception {
+	public void should_be_default_test_3() throws Exception {
 		/* following the example on https://testsmells.github.io/pages/testsmellexamples.html#DefaultTest*/
-		Calculate calcule = new Calculate();
+		Calculate calcule = new Calculate("on");
 		int actual = calcule.add(2, 3);
 		int expected = 2;
 		assertEquals(4, 2 + 2);
 	}
 
 	@Test
-	public void unknown_test() {
-		Calculate calcule = new Calculate();
+	public void should_be_unknown_test() {
+		Calculate calcule = new Calculate("on");
 		int actual = calcule.add(2, 3);		
 	}
 	
 	@Test
-	public void empty_test() {
+	public void should_be_empty_test() {
 	}
 	
 	@Test
-	public void print_statement_one() {
+	public void should_be_print_statement_one() {
 		System.out.println("test with println");
 	}
 	
 	@Test
-	public void print_statement_two() {
+	public void should_be_print_statement_two() {
 		int a = 1;
 		int b = 2;
 		System.out.printf("test with printf = %d",(a + b));
 	}
 	
 	@Test
-	public void print_statement_three() {
+	public void should_be_print_statement_three() {
 		System.out.print("test with print");
 	}
 	
 	@Test
-	public void conditional_one() {
+	public void should_be_conditional_one() {
 		if (true) {
 		}	
 	}
 	
 	@Test
-	public void conditional_two() {
+	public void should_be_conditional_two() {
 		for (int i = 0; i < 2; i++) {
 		}		
 	}
+	
 	@Test
-	public void conditional_three() {
+	public void should_be_conditional_three() {
 		int i = 0;
 		while (i < 2) {
 			i++;
 		}
 	}
+	
 	@Test
-	public void conditional_four() {
+	public void should_be_conditional_four() {
 		int i = 0;
 		System.out.println((i <= 2) ? i : 2);
 	}
 	@Test
-	public void conditional_five() {
+	public void should_be_conditional_five() {
 		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 		for (String i : cars) {
 		  System.out.println(i);
 		}	
 	}
 	@Test
-	public void conditional_six() {
+	public void should_be_conditional_six() {
 		int day = 4;
 		switch (day) {
 		  case 1:
@@ -112,7 +120,7 @@ public class CalculateTest {
 		}		
 	}
 	@Test
-	public void conditional_seven() {
+	public void should_be_conditional_seven() {
 		int i = 0;
 		do {
 		  System.out.println(i);
@@ -126,17 +134,17 @@ public class CalculateTest {
 		
 	}
 	@Ignore @Test
-	public void ignored_test_two() {
+	public void should_be_ignored_test_two() {
 		System.out.println("Runnig a test without @Test");
 	}
 	
 	@Test @Ignore 
- 	public void ignored_test_three() {
+ 	public void should_be_ignored_test_three() {
 		System.out.println("Runnig a test without @Test");
 	}
 	
 	@Test
-	public void expection_one() {
+	public void should_be_expection_one() {
 		try {
 			throw new Error("oops");
 	     }
@@ -157,35 +165,35 @@ public class CalculateTest {
 
 	@Test
 	public void should_not_be_eager_test() {
-		Calculate calcule = new Calculate();
+		Calculate calcule = new Calculate("on");
 		assertEquals(calcule.turnOn(),"");
-		assertThat(calcule.turnOn(), is("dada"));
+		assertThat(calcule.turnOn(), is(""));
 	}
 	
 	@Test
 	public void should_be_eager_test() {
-		Calculate calcule = new Calculate();
+		Calculate calcule = new Calculate("off");
 		assertThat(calcule.turnOn(), is(""));
 		assertThat(calcule.turnOn(), is(""));
 	}
 	
 	@Test
-	public void should_be_eager_test_two() {
-		Calculate calcule = new Calculate();
+	public void should_not_be_eager_test_two() {
+		Calculate calcule = new Calculate("on");
 		assertEquals(calcule.turnOn(),"");
 		assertEquals(calcule.turnOff(),"");
 	}
 	
 	@Test
 	public void should_be_lazy_test() {
-		Calculate calcule = new Calculate();
+		Calculate calcule = new Calculate("on");
 		assertEquals(calcule.add(3, 5),calcule.add(3, 5));
 		assertEquals(calcule.add(2, 5),calcule.add(2, 5));
 	}
 	
 	@Test
 	public void should_be_lazy_test_two() {
-		Calculate calcule = new Calculate();
+		Calculate calcule = new Calculate("on");
 		int count_one = calcule.add(3, 5)+ calcule.add(3, 5);
 		int count_two = calcule.add(3, 5)+ calcule.add(3, 5);
 		assertEquals(count_one, count_two);
@@ -194,7 +202,7 @@ public class CalculateTest {
 	@Test
 	public void should_be_assertion_roulette() {
 		// and duplicate assert
-		Calculate calcule = new Calculate();
+		Calculate calcule = new Calculate("on");
 		assertEquals(calcule.turnOn(),"");
 		assertEquals(calcule.turnOff(),"");
 		assertEquals(calcule.turnOff(),"");
@@ -215,12 +223,6 @@ public class CalculateTest {
 	}
 	
 	@Test
-	public void should_be_construct_initialization_two() {
-		int value = calc.add(2, 3);
-		assertEquals(value,2);
-	}
-	
-	@Test
 	public void should_be_mistery_number() {
 		assertEquals(calc.add(2, 3),5);
 	}
@@ -234,6 +236,46 @@ public class CalculateTest {
 	private Matcher is(String string) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Test
+	public void should_be_Mistery_Guest() throws IOException{
+	    File tempFile = File.createTempFile("test", ".txt");
+	}
+	
+	@Test
+	public void should_be_resource_optimism() throws Exception {
+		File file = new File( "file.txt" );
+	    FileWriter fw = new FileWriter( file );
+	    fw.write("my text");
+	    fw.close();
+	    file.delete();
+	}
+	
+	@Test
+	public void should_not_be_resource_optimism() throws Exception {
+		File file = new File( "file.txt" );
+	    if(file.exists()) {
+	    	FileWriter fw = new FileWriter( file );
+	    	fw.write("my text");
+	    }
+	}
+
+	
+	@Test
+	public void should_be_redundant_assertion() {
+	    assertEquals(true, true);
+	}
+	
+	@Test
+	public void should_be_redundant_assertion_two() {
+	    assertEquals(false, false);
+	}
+	
+	@Test
+	public void should_be_sensitive_equality() {
+		String str = "my string"; 
+		assertEquals(str.toString(), "4");
 	}
 
 }
